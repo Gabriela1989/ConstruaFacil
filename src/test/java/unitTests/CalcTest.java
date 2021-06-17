b391e49822f53b5a352943707a44d00d33c27738
dell <gabrielalima16@gmail.com>
2021-06-17 18:18:07 -0300

    Commit 07 - Criei o teste subtrairDoisNumeros, multiplicarDoisNumeros e dividirDoisNumeros.

--- src/test/java/unitTests/CalcTest.java
+++ src/test/java/unitTests/CalcTest.java
@@ -5,22 +5,79 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
+
 public class CalcTest {
-    @Test
+    @Test(priority = 1)
+
     public void testarSomarDoisNumeros() {
-        // 1 - Prepara - Configurar - Given
+     // 1 - Prepara - Configurar - Given - Arrange
+
         int num1 = 5;
         int num2 = 7;
         int resultadoEsperado = 12;
 
-        // 2 - Executar - When
+     // 2 - Executar - When - Act
         int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
 
-        // 3 - Validar - Then
+    // 3 - Validar - Then - Assert
+        System.out.println("O resultado esperado seria " + resultadoEsperado + " e o resultado atual foi " + resultadoAtual);
 
-        System.out.println("O resultado Esperado é " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
-
-        assertEquals(resultadoEsperado, resultadoAtual);
+        assertEquals(resultadoAtual, resultadoEsperado);
 
     }
-}
+
+    @Test(priority = 2)
+    public void testarSubtrairDoisNumeros(){
+
+        int num1 = 6;
+        int num2 = 2;
+        int resultadoEsperado = 4;
+
+        int resultadoAtual = Calc.subtrairDoisNumeros(num1,num2);
+
+        System.out.println("O resultado esperado seria " + resultadoEsperado
+                + " e o resultado atual foi " + resultadoAtual);
+
+        assertEquals(resultadoAtual,resultadoEsperado);
+
+    }
+
+    @Test(priority = 3)
+
+    public void testarmultiplicarDoisNumeros(){
+
+        int num1 = 6;
+        int num2 = 6;
+        int resultadoEsperado = 36;
+
+        int resultadoAtual = Calc.multiplicarDoisNumeros(num1,num2);
+
+        System.out.println("O resultado esperado seria " + resultadoEsperado
+                + " e o resultado atual foi " + resultadoAtual);
+
+        assertEquals(resultadoAtual,resultadoEsperado);
+
+     }
+
+    @Test(priority = 4)
+
+    public void testardividirDoisNumeros(){
+        int num1 = 100;
+        int num2 = 2;
+        int resultadoEsperado = 50;
+
+        int resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
+
+        System.out.println("O resultado esperado seria " + resultadoEsperado
+                + " e o resultado atual foi " + resultadoAtual);
+
+        assertEquals(resultadoAtual,resultadoEsperado);
+
+    }
+
+
+    }
+
+
+
+
